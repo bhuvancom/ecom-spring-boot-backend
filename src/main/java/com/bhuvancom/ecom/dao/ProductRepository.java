d902deb54c93a204cd79d0fd5b78a57ca5d34a85
bhuvaneshvar <bhuvaneshvar6@outlook.com>
2021-03-07 17:05:46 +0530

    v1

--- src/main/java/com/bhuvancom/ecom/dao/ProductRepository.java
+++ src/main/java/com/bhuvancom/ecom/dao/ProductRepository.java
@@ -2,8 +2,8 @@ package com.bhuvancom.ecom.dao;
 
 import com.bhuvancom.ecom.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@Repository
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
